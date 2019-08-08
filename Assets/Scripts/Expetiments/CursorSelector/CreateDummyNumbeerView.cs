2ad3b1d5b3894320fae1e7f12bef0e0dcb410e2e
tkdsym2 <k222.tkd.fms@gmail.com>
2019-08-09 03:20:53 +0900

    Feat: add Cursor Number Viweer
    タスク終了時にスペースキーを押して時を止め，実験者に報告する際に必要な数字を描画

---
+++ Assets/Scripts/Expetiments/CursorSelector/CreateDummyNumbeerView.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class CreateDummyNumbeerView : MonoBehaviour
+{
+    private StudyManager sm;
+    public GameObject textPrefab;
+    private GameObject[] text;
+    public GameObject dummies;
+    private float drx, dry, drangle;
+    private int counter;
+    public Canvas selectPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = GameObject.Find("StudyManager").GetComponent<StudyManager>();
+        text = new GameObject[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(sm.isStartSession)
+        {
+            counter = 0;
+            foreach(Transform child in dummies.transform) {
+                Vector3 pos = Camera.main.WorldToScreenPoint(new Vector3(child.transform.position.x, child.transform.position.y, 0));
+                pos.x = pos.x - Screen.width / 2 + 30;
+                pos.y = pos.y - Screen.height / 2 + 20;
+                text[counter].GetComponent<RectTransform>().localPosition = pos;
+                if(sm.dummySelectableNumbers.Count != 0){
+                    int randomNumber = UnityEngine.Random.Range(0, sm.dummySelectableNumbers.Count);
+                    text[counter].GetComponent<Text>().text = sm.dummySelectableNumbers[randomNumber].ToString();
+                    sm.dummySelectableNumbers.RemoveAt(randomNumber);
+                }
+                counter++;
+            }
+        }
+    }
+
+    public void InitDummyNumberView()
+    {
+        text = new GameObject[sm.dummyNum];
+        GenerateDummyNumberView(sm.dummyNum);
+    }
+
+    public void AllClear()
+    {
+        Array.Clear(text, 0, text.Length);
+    }
+
+    private void GenerateDummyNumberView(int _num)
+    {
+        for(int i = 0; i < _num; i++)
+        {
+            text[i] = Instantiate(textPrefab) as GameObject;
+            text[i].transform.SetParent(selectPanel.transform);
+        }
+    }
+
+    public void DestroyDummyView()
+    {
+        for(int i = 0; i < text.Length; i++)
+        {
+            DestroyImmediate(text[i]);
+        }
+    }
+}
---
+++ Assets/Scripts/Expetiments/CursorSelector/CreateUserCursorNumberView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateUserCursorNumberView : MonoBehaviour
+{
+    public GameObject userCursor;
+    public GameObject studyManager;
+    private StudyManager sm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = studyManager.GetComponent<StudyManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(sm.isStartSession)
+        {
+            float x = userCursor.transform.position.x;
+            float y = userCursor.transform.position.y;
+            Vector3 pos = Camera.main.WorldToScreenPoint(userCursor.transform.position);
+            pos.x = pos.x - Screen.width/2 + 30;
+            pos.y = pos.y - Screen.height/2 + 20;
+            this.GetComponent<RectTransform>().localPosition = pos;
+            this.GetComponent<Text>().text = sm.selfCursorNum.ToString();
+        }
+    }
+}
