cc3dad7a5661f79ed6e79a23ba2650473023c69e
tkdsym2 <k222.tkd.fms@gmail.com>
2019-08-23 05:31:51 +0900

    Feat: add DataManagerClass
    １つ前のSceneをデフォルトで取得できないので，それ専用のclassを追加

---
+++ Assets/Scripts/Expetiments/Public/DataManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager
+{
+    public readonly static DataManager Instance = new DataManager();
+    public string PreviousScene = string.Empty;
+}
