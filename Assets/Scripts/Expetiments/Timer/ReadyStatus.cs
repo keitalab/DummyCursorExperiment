9d084ac7933924528c9b8ce1ec38679e481270df
tkdsym2 <k222.tkd.fms@gmail.com>
2019-08-23 14:14:30 +0900

    Feat: add text view for ready state
    本番前用確認テキスト

---
+++ Assets/Scripts/Expetiments/Timer/ReadyStatus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReadyStatus : MonoBehaviour
+{
+    public GameObject _StudyManager;
+    private StudyManager studyManager;
+    private Text ReadyStateText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        studyManager = _StudyManager.GetComponent<StudyManager>();
+        ReadyStateText = gameObject.GetComponent<Text>();
+        ReadyStateText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!studyManager.isPractice && !studyManager.isReady) ReadyStateText.text = "Are you ready ?";
+            else ReadyStateText.text = "";
+    }
+}
